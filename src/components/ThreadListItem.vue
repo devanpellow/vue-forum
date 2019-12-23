e1a264529d2f3a0c93e844b8b1f1ca722901a722
Devan Pellow <pellod01@bitbucket.org>
2019-12-23 13:43:59 +0100

    created ThreadList and ThreadListItem to display all threads in a list

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{thread.title}}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+          By <a href-"#">Joseph Kerr</a>, {{thread.publishedAt}}.
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{Object.keys(thread.posts).length-1}} replies
+      </p>
+      
+      <!-- <img class="avatar-medium" src="" alt=""> -->
+      
+      <!-- <div>
+        <p class="text-xsmall">
+          <a href="#">Bruce Wayne</a>
+        </p>
+        <p class="text-xsmall text-faded">
+          2 hours ago
+        </p>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      thread: {
+        required: true,
+        type: Object
+      }
+    }
+  }
+</script>
