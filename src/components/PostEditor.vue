b90d3c1ea8cc95ecb5bc1b482851fc2cda304c1f
devanpellow <devanpellow@gmail.com>
2019-12-24 10:26:29 +0100

    added PostEditor component to add comments to threads

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,69 +1,51 @@
 <template>
   <div>
-    <div class="col-large push-top">
-      <h1>{{thread.title}}</h1>
-      <PostList :posts="posts"/>
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea 
-          class="form-input"
-          name=""
-          id="" 
-          cols="30" 
-          rows="10"
-          v-model="newPostText"
-          ></textarea>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">Submit Post</button>
-        </div>
-      </form>
-    </div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea 
+        class="form-input"
+        name=""
+        id="" 
+        cols="30" 
+        rows="10"
+        v-model="text"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit Post</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import PostList from '@/components/PostList'
 
 export default {
-  components: {
-    PostList
-  },
   props: {
-    id: {
-      required: true,
-      type: String
+    threadId: {
+      required: true
     }
   },
 
   data () {
     return {
-      thread: sourceData.threads[this.id],
-      newPostText: ''
-    }
-  },
-
-  computed: {
-    posts () {
-      const postsIds = Object.values(this.thread.posts)
-      return Object.values(sourceData.posts).filter(post => postsIds.includes(post['.key']))
+      text: ''
     }
   },
   methods: {
-    addPost () {
+    save () {
       const postId = 'greatPost' + Math.random()
       const post = {
-        text: this.newPostText,
+        text: this.text,
         publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
+        threadId: this.threadId,
         userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
         '.key': postId
       }
-      this.$set(sourceData.posts, postId, post)
-      this.$set(this.thread.posts, postId, postId)
-      this.$set(sourceData.users[post.userId].posts, postId, postId)
-      this.newPostText = ''
+      this.text = ''
+
+      this.$emit('save', {post})
     }
   }
 }
