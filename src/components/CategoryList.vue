85b8aac8855abf66d2e21c4c40a88bc3d23a7dd7
devanpellow <devanpellow@gmail.com>
2019-12-27 13:50:50 +0100

    created category list and item components

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <CategoriesListItem v-for="category in categories" :category="category" :key="category['.key']" />
+  </div>
+</template>
+
+<script>
+import CategoriesListItem from "./CategoryListItem";
+
+export default {
+  components: {
+    CategoriesListItem
+  },
+
+  props: {
+    categories: {
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
