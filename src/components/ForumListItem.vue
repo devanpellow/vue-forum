f8441d62a544150fec2630de4898bf6c64f122bd
devanpellow <devanpellow@gmail.com>
2019-12-27 12:30:52 +0100

    changed homepage to show list of forums and links to forums and their threads

---
+++ src/components/ForumListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link 
+      class="text-xlarge"
+      :to="{name: 'Forum', params: {id: forum['.key']}}"
+      >
+        {{forum.name}}
+      </router-link>
+      <p>{{forum.description}}</p>
+    </div>
+    <div class="thread-count">
+      <p class="count">{{threadCount}}</p>{{threadCount === 1 ? 'thread' : 'threads'}}
+    </div>
+    <div class="last-thread">
+      <!-- some todo -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    threadCount () {
+      return this.forum.threads ? Object.values(this.forum.threads).length : 0
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import PageNotFound from '@/pages/PageNotFound'
+import PageForum from '@/pages/PageForum'
 
 Vue.use(Router)
 
@@ -22,6 +23,12 @@ export default new Router({
       path: '/thread/:id',
       name: 'ThreadShow',
       component: ThreadShow,
+      props: true
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: PageForum,
       props: true
     }
   ],
