9e0042557898188b3c23cc87009a0001da41bdd7
devanpellow <devanpellow@gmail.com>
2019-12-30 17:17:50 +0100

    authenticated user not shows when commenting on thread and added icon

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -29,15 +29,10 @@ export default {
       type: String
     }
   },
-
-  data () {
-    return {
-      thread: this.$store.state.threads[this.id],
-      text: ''
-    }
-  },
-
   computed: {
+    thread () {
+      return this.$store.state.threads[this.id]
+    },
     posts () {
       const postsIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts).filter(post => postsIds.includes(post['.key']))
