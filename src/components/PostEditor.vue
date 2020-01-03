6129e56ab630df3802df2f61588a1bf9f12a3ee6
devanpellow <devanpellow@gmail.com>
2020-01-03 17:38:20 +0100

    store thread in firebase - fixed post prop error

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -1,85 +1,85 @@
 <template>
-  <div>
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <textarea 
-        class="form-input"
-        name=""
-        id="" 
-        cols="30" 
-        rows="10"
-        v-model="text"
+  <form @submit.prevent="save">
+    <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+          v-model="text"
         ></textarea>
-      </div>
-      <div class="form-actions">
-        <button v-if="isUpdate" @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn-blue">{{isUpdate? 'Update' : 'Submit Post'}}</button>
-      </div>
-    </form>
-  </div>
+    </div>
+    <div class="form-actions">
+      <button v-if="isUpdate" @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
+      <button class="btn-blue">{{isUpdate ? 'Update' : 'Submit post'}}</button>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
-  props: {
-    threadId: {
-      required: false
-    },
-    post: {
-      type: Object,
-      validator: obj => {
-        const keyIsValid = typeof obj['.key'] === 'string'
-        const textIsValid = typeof obj.text === 'string'
-        const valid = keyIsValid && textIsValid
-        if (!textIsValid) {
-          console.error('The post prop object must include a `text` attribute.')
+    export default {
+      props: {
+        threadId: {
+          required: false
+        },
+        post: {
+          type: Object,
+          validator: obj => {
+            const keyIsValid = typeof obj['.key'] === 'string'
+            const textIsValid = typeof obj.text === 'string'
+            const valid = keyIsValid && textIsValid
+            if (!textIsValid) {
+              console.error('😳 The post prop object must include a `text` attribute.')
+            }
+            if (!keyIsValid) {
+              console.error('😳 The post prop object must include a `.key` attribute.')
+            }
+            return valid
+          }
         }
-        if (!keyIsValid) {
-          console.error('The post prop object must include a `key` attribute.')
+      },
+      data () {
+        return {
+          text: this.post ? this.post.text : ''
         }
-        return valid
+      },
+      computed: {
+        isUpdate () {
+          return !!this.post
+        }
+      },
+      methods: {
+        save () {
+          this.persist()
+            .then(post => {
+              this.$emit('save', {post})
+            })
+        },
+        cancel () {
+          this.$emit('cancel')
+        },
+        create () {
+          const post = {
+            text: this.text,
+            threadId: this.threadId
+          }
+          this.text = ''
+          return this.$store.dispatch('createPost', post)
+        },
+        update () {
+          const payload = {
+            id: this.post['.key'],
+            text: this.text
+          }
+          return this.$store.dispatch('updatePost', payload)
+        },
+        persist () {
+          return this.isUpdate ? this.update() : this.create()
+        }
       }
     }
-  },
-
-  data () {
-    return {
-      text: this.post ? this.post.text : ''
-    }
-  },
-  computed: {
-    isUpdate () {
-      return !!this.post
-    }
-  },
-  methods: {
-    save () {
-      this.persist()
-      .then(post => {
-        this.$emit('save', {post})
-      })
-    },
-    cancel () {
-      this.$emit('cancel')
-    },
-    create () {
-      const post = {
-        text: this.text,
-        threadId: this.threadId
-      }
-      this.text = ''
-      return this.$store.dispatch('createPost', post)
-    },
-    update () {
-      const payload = {
-        id: this.post['.key'],
-        text: this.text
-      }
-      return this.$store.dispatch('updatePost', payload)
-    },
-    persist () {
-      return this.isUpdate ? this.update() : this.create()
-    }
-  }
-}
 </script>
+
+<style scoped>
+</style>
--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -2,6 +2,7 @@
   <div v-if="forum" class="col-full push-top">
 
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
+
     <ThreadEditor
       @save="save"
       @cancel="cancel"
@@ -10,38 +11,42 @@
 </template>
 
 <script>
-  import {mapActions} from 'vuex'
-  import ThreadEditor from '@/components/ThreadEditor'
-
-  export default {
-    components: {
-      ThreadEditor
-    },
-    props: {
-      forumId: {
-        type: String,
-        required: true
-      }
-    },
-    methods: {
-      ...mapActions(['createThread', 'fetchForum']),
-      save ({title, text}) {
-        this.createThread({
-          forumId: this.forum['.key'],
-          title,
-          text
-        }).then(thread => {
-          this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
-        })
+    import {mapActions} from 'vuex'
+    import ThreadEditor from '@/components/ThreadEditor'
+    export default {
+      components: {
+        ThreadEditor
       },
-      cancel (thread) {
-        this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
+      props: {
+        forumId: {
+          type: String,
+          required: true
+        }
+      },
+      computed: {
+        forum () {
+          return this.$store.state.forums[this.forumId]
+        }
+      },
+      methods: {
+        ...mapActions(['createThread', 'fetchForum']),
+        save ({title, text}) {
+          this.createThread({
+            forumId: this.forum['.key'],
+            title,
+            text
+          }).then(thread => {
+            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
+          })
+        },
+        cancel () {
+          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
+        }
+      },
+      created () {
+        this.fetchForum({id: this.forumId})
       }
-    },
-    created () {
-      this.fetchForum({id: this.forumId})
     }
-  }
 </script>
 
 <style scoped>
