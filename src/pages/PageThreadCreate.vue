fb535b90f21ac462ed383ddb1bab5436e82266f7
devanpellow <devanpellow@gmail.com>
2020-01-06 17:12:51 +0100

    added mixins now components wait until data is fetched

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="forum" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
 
     <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
@@ -11,42 +11,45 @@
 </template>
 
 <script>
-    import {mapActions} from 'vuex'
-    import ThreadEditor from '@/components/ThreadEditor'
-    export default {
-      components: {
-        ThreadEditor
-      },
-      props: {
-        forumId: {
-          type: String,
-          required: true
-        }
-      },
-      computed: {
-        forum () {
-          return this.$store.state.forums[this.forumId]
-        }
-      },
-      methods: {
-        ...mapActions(['createThread', 'fetchForum']),
-        save ({title, text}) {
-          this.createThread({
-            forumId: this.forum['.key'],
-            title,
-            text
-          }).then(thread => {
-            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
-          })
-        },
-        cancel () {
-          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
-        }
-      },
-      created () {
-        this.fetchForum({id: this.forumId})
+  import {mapActions} from 'vuex'
+  import ThreadEditor from '@/components/ThreadEditor'
+  import asyncDataStatus from '@/mixins/asyncDataStatus'
+  export default {
+    components: {
+      ThreadEditor
+    },
+    mixins: [asyncDataStatus],
+    props: {
+      forumId: {
+        type: String,
+        required: true
       }
+    },
+    computed: {
+      forum () {
+        return this.$store.state.forums[this.forumId]
+      }
+    },
+    methods: {
+      ...mapActions(['createThread', 'fetchForum']),
+      save ({title, text}) {
+        this.createThread({
+          forumId: this.forum['.key'],
+          title,
+          text
+        }).then(thread => {
+          this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
+        })
+      },
+      cancel () {
+        this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
+      }
+    },
+    created () {
+      this.fetchForum({id: this.forumId})
+        .then(() => { this.asyncDataStatus_fetched() })
     }
+  }
 </script>
 
 <style scoped>
