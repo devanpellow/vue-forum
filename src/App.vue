829c09f4e1289e1e21f86f65e82a5f046ea4218a
devanpellow <devanpellow@gmail.com>
2020-01-06 18:03:44 +0100

    added progress loading bar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <TheNavbar />
     <div class="container">
-      <router-view v-show="showPage" @ready="showPage = true"/>
+      <router-view v-show="showPage" @ready="pageReady"/>
       <AppSpinner v-show="!showPage" />
     </div>
   </div>
@@ -11,6 +11,7 @@
 <script>
 import TheNavbar from '@/components/TheNavbar'
 import AppSpinner from '@/components/AppSpinner'
+import NProgress from 'nprogress'
 export default {
   components: {
     TheNavbar,
@@ -21,9 +22,21 @@ export default {
       showPage: false
     }
   },
+  methods: {
+    pageReady () {
+      this.showPage = true
+      NProgress.done()
+    }
+  },
   created () {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false
+    })
+    NProgress.start()
     this.$router.beforeEach((to, from, next) => {
       this.showPage = false
+      NProgress.start()
       next()
     })
   }
@@ -31,5 +44,8 @@ export default {
 </script>
 
 <style>
-
+  @import "~nprogress/nprogress.css";
+  #nprogress .bar {
+    background: #57AD8D
+  };
 </style>
