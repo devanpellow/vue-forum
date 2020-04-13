61c20302876e46e433c94aedbadadc480a04a03e
devanpellow <devanpellow@gmail.com>
2020-04-13 22:37:57 +0200

    fetch data for all pages

--- src/pages/PageCategory.vue
+++ src/pages/PageCategory.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="col-full">
+  <div v-if="category" class="col-full">
     <h1>{{ category.name }}</h1>
     <CategoryListItem :category="category"/>
   </div>
 </template>
 
 <script>
-import CategoryListItem from '@/components/CategoryListItem'
-
-export default {
-  components: {
-    CategoryListItem
-  },
-  props: {
-    id: {
-      required: true,
-      type: String
+    import CategoryListItem from '@/components/CategoryListItem'
+    export default {
+      components: {
+        CategoryListItem
+      },
+      props: {
+        id: {
+          required: true,
+          type: String
+        }
+      },
+      computed: {
+        category () {
+          return this.$store.state.categories[this.id]
+        }
+      },
+      created () {
+        this.$store.dispatch('fetchCategory', {id: this.id})
+          .then(category => {
+            this.$store.dispatch('fetchForums', {ids: category.forums})
+          })
+      }
     }
-  },
-  computed: {
-    category () {
-      return this.$store.state.categories[this.id]
-    }
-  }
-}
 </script>
 
 <style scoped>
-
-</style>>
+</style>
