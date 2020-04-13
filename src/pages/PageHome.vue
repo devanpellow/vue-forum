0f48214f4630bb842f4dea0e587ef319ef65799a
devanpellow <devanpellow@gmail.com>
2020-04-13 23:34:59 +0200

    share login between components with mixins

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="ready" class="col-full push-top">
+  <div v-if="asyncDataStatus_ready" class="col-full push-top">
     <h1>Welcome to the Forum</h1>
     <CategoryList :categories="categories"/>
   </div>
@@ -8,15 +8,12 @@
 <script>
 import {mapActions} from 'vuex'
 import CategoryList from '@/components/CategoryList'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 export default {
   components: {
     CategoryList
   },
-  data () {
-    return {
-      ready: false
-    }
-  },
+  mixins: [asyncDataStatus],
   computed: {
     categories () {
       return Object.values(this.$store.state.categories)
@@ -29,7 +26,7 @@ export default {
     this.fetchAllCategories()
       .then(categories => Promise.all(categories.map(category => this.fetchForums({ids: Object.keys(category.forums)}))))
       .then(() => {
-        this.ready = true
+        this.asyncDataStatus_fetched()
       })
   }
 }
