dfc67219b7bef63484d23402fa74adc308d09f8a
devanpellow <devanpellow@gmail.com>
2020-04-14 02:50:19 +0200

    Using .env file in the Vue.js Webpack Template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,14 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_API_KEY: "'AIzaSyChTSb8a5xetN0TWFV5lla5ZGKtQv_m234'",
-  FIREBASE_AUTH_DOMAIN: "'forum-aa68a.firebaseapp.com'",
-  FIREBASE_DATABASE_URL: "'https://forum-aa68a.firebaseio.com'",
-  FIREBASE_PROJECT_ID: "'forum-aa68a'",
-  FIREBASE_STORAGE_BUCKET: "'forum-aa68a.appspot.com'",
-  FIREBASE_MESSAGING_ID: "'706176004849'",
-  FIREBASE_APP_ID: "'1:706176004849:web:158569204c9c4e88f76f50'",
-  FIREBASE_MEASUREMENT_ID: "'G-BY4T56ZBZY'"
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID),
+  FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
+  FIREBASE_MEASUREMENT_ID: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
 }
